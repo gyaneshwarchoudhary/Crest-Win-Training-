32944d649d2cbba7148beb6946c5ece29523dadf
gyaneshwarchoudhary <gyaneshwarchoudhary637@gmail.com>
2026-02-04 20:06:25 +0530

    Day-04 topics completed

---
+++ Day-04/__Doc__.py
@@ -0,0 +1,43 @@
+"""
+Module for Day 04 tasks.
+ - OOP in python
+ - pep8 and clear code
+ - logging and debugging
+ - linux processes
+"""
+
+"""
+OOP concepts:
+ - programming paradigm
+ - structure, bheavior, scalable and reusable
+ - Access specifiers: 
+ - key concepts:
+    - class: blueprint, constructors and types
+    - object: instance of class
+    - inheritance: code reuse and hierarchy
+    - encapsulation: modular and secure code 
+    - polymorphism: multiple forms
+    - abstraction: hiding the implementation 
+ - 
+"""
+
+
+"""
+Python Enhancement proposals (PEP8):
+ - styling guide, maintainable and collaboration
+ - Key Guidelines:
+    - indentation: 4 spaces
+    - line length: 79 characters
+    - blank lines: 2 for top level, 1 for methods
+    - imports: each import on separate line
+    - naming conventions: snake_case for functions and variables, CamelCase for classes
+    - whitespace: avoid extra spaces
+    - comments and docstrings: use them effectively
+"""
+
+"""
+Logging and Debugging:
+ - track events
+ - catagorisation, Destination, context, control
+ - 5 levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
+""" 
