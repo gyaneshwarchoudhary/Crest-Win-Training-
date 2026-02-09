cec7fb55a9202c37949f6977209c05bdb0965faa
gyaneshwarchoudhary <gyaneshwarchoudhary637@gmail.com>
2026-02-09 18:40:05 +0530

    Day-08 Started

---
+++ Day-08/__Doc__.py
@@ -0,0 +1,16 @@
+"""
+Docstring for Day-08.__Doc__
+• FastAPI	project	setup
+• Routing
+• Pydantic	models
+• Dependency	injection
+"""
+
+
+"""
+FastAPI:
+    FastAPI is a modern, fast(high performance) web framework
+    features of fastapi:
+        - fast, fewer bugs, Intuitive, easy, short, robust, standard based
+        - 
+"""
