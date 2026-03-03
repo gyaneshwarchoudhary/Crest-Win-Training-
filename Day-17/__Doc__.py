0478b0281f3e627d0a0a4b13ff4ced688bd19241
gyaneshwarchoudhary <gyaneshwarchoudhary637@gmail.com>
2026-03-03 19:06:25 +0530

    Day-18 partially done

---
+++ Day-17/__Doc__.py
@@ -0,0 +1,48 @@
+"""
+Day 17
+• Selenium	fundamentals
+• Selectors
+• UI	flows
+• Automation	mindset
+"""
+
+"""
+Selector:
+  - DOM (Document Object Model)
+  - CSS Selectors and XPath
+  - id, class, name, tag, link text, partial link text, CSS selector, XPath
+"""
+
+"""
+UI Flows:
+  - selector
+  - xpath(address)
+  - Ui flows (journey)
+  - Selenium WebDriver
+"""
+
+
+"""
+Automation Mindset:
+  - Identify repetitive tasks
+  - Design test 
+  - prioritize high value automation 
+  - Build automation as part of development, not after
+  - Think like an Engineer not a tester
+  - eliminate manual work
+
+"""
+
+
+"""
+Selenium:
+   - open-source framework for automating web browsers
+   - Automating repetitive browser tasks.
+   - Testing web applications for functionality, regression and performance.
+   - Web scraping and data extraction from dynamic web pages.
+   - Integrating with CI/CD pipelines for automated testing.
+   Selenium WebDriver
+   selenium IDE
+   slenium grid
+   
+"""
---
+++ Day-18/__Doc__.py
@@ -0,0 +1,56 @@
+"""
+Day 18
+• Playwright	basics
+• Modern	UI	testing
+• Headless	runs
+• CI-friendly	tests
+"""
+
+"""
+software Testing:
+    - important phase of software development lifecycle
+    types of software testing:
+      manual and automation
+    - manual testing:
+        - white box testing
+        - black box testing
+        - grey box testing
+      black box testing:
+        - functional testing
+           - unit testing
+           - integration testing
+              - incremental testing
+                    - top down
+                    - bottom up
+               - non incremental testing
+                    - big bang testing
+
+           - system testing
+        - non-functional testing
+             - performance testing
+                - load testing
+                - stress testing
+                - scalability testing
+                - stability testing
+             - security testing
+             - usability testing
+             - compatibility testing
+
+
+"""
+
+"""
+ Test Types:
+    - Functional Testing
+    - non-functional testing
+    - smoke` testing
+    - sanity testing
+    - regression testing
+    - usability testing
+    - performance testing
+    - security testing
+    - compatibility testing
+    - exploratory testing
+    
+
+"""
